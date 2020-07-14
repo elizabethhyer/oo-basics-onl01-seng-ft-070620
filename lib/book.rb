ef0dba76d519515f26257434605cac938637b20b
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-14 14:15:01 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -6,10 +6,6 @@ class Book
     @title = title
   end
   
-  def title
-    @title
-  end 
-  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end 
@@ -18,5 +14,3 @@ end
 
 book = Book.new("And Then There Were None")
 book.turn_page
-
-#learn spec/01_book_spec.rb
