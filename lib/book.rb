81d7f816ffa38279b24cd15e45da28f11c6b17ab
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-14 14:05:02 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,33 @@
+class Book
+  
+  def initialize(title)
+    @title = title
+  end
+  
+  def title
+    @title
+  end 
+  
+  def author=(author)
+    @author = author
+  end 
+  
+end 
+
+Book.new("And Then There Were None")
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+#learn spec/01_book_spec.rb
